c1241cf0bd1f2059087985604c64b0ce5bcd0567
xinyi-jane <your-email@example.com>
2025-12-19 19:40:55 +0800

    修复 base 路径配置

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
   integrations: [mdx(), tailwind()],
   site: 'https://xinyi-jane.github.io',
-  base: '/',
+  base: '/blog/',
   output: 'static',
 });
 
